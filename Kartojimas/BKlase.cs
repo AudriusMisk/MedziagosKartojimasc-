2ea56200f171dfee8adb796df6c730a3bb15de96
Audrius Miskinis <audrius.miskinis@mif.stud.vu.lt>
2021-04-26 21:23:39 +0300

    kartojimas

---
+++ Kartojimas/BKlase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kartojimas
+{
+    public class BKlase: BaseClass, IZodisKitaip
+    {
+        public BKlase(string name) : base(name)
+        {
+        }
+
+        public override int NameKodas(int name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string NameMetodas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ZodisBeBalsiu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ZodisBePriebalsiu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ZodisSuPakeistomisBalsemis(char a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ZodisSuPakeistomisPriebalsemis(int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Kartojimas/IZodisKitaip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kartojimas
+{
+    public interface IZodisKitaip
+    {
+        string ZodisBeBalsiu();
+        string ZodisBePriebalsiu();
+        string ZodisSuPakeistomisBalsemis(char a);
+        string ZodisSuPakeistomisPriebalsemis(int b);
+
+    }
+}
