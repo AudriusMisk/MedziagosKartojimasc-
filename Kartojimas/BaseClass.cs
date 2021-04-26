96ced1fce2132719d0d68d21b7d4c744150cad84
Audrius Miskinis <audrius.miskinis@mif.stud.vu.lt>
2021-04-26 20:21:35 +0300

    Add project files.

---
+++ Kartojimas/BaseClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kartojimas
+{
+    abstract class BaseClass
+    {
+        protected BaseClass(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public abstract string NameMetodas();
+
+        public abstract int NameKodas(int name);
+    }
+}
