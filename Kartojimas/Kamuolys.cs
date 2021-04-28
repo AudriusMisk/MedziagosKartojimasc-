d494cf1cabe93947584b7ac41fbda1de7105d853
Audrius Miskinis <audrius.miskinis@mif.stud.vu.lt>
2021-04-28 20:15:07 +0300

    kartojimas

---
+++ Kartojimas/Kamuolys.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kartojimas
+{
+    public class Kamuolys : Spalva
+    {
+        public Kamuolys(int dydis, int metimuSkaicius)
+        {
+            Dydis = dydis;
+            MetimuSkaicius = metimuSkaicius;
+        }
+
+        public int Dydis { get; private set; }
+        public int MetimuSkaicius { get; set; }
+
+        public void Mesti()
+        {
+            MetimuSkaicius ++ ;
+        }
+
+        public void Susprogdinti()
+        {
+            Dydis++ ;
+        }
+
+    }
+}
